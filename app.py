f5c005b65ca99139c2e7cd5489e53234539f20af
SaninSad <sanin.sadikovic@student.ipso.ch>
2025-03-20 11:40:15 +0100

    Update app.py comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import der benötigten Flask-Module
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -5,19 +6,26 @@ from flask_login import LoginManager
 from config import Config
 from flask_jwt_extended import JWTManager
 
-
+# Flask-Anwendung initialisieren
 app = Flask(__name__)
+# Konfiguration aus der Config-Klasse laden
 app.config.from_object(Config)
 
+# Initialisierung der Datenbank und Migration
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# Flask-Login initialisieren
 login = LoginManager(app)
 login.login_view = 'login'
 
+# Konfiguration des JWT-Authentifizierungssystems
 app.config['JWT_SECRET_KEY'] = 'dein_geheimer_schlüssel'
 jwt = JWTManager(app)  # ✅ JWT Manager Initialisieren
 
+# Import der Routen für die Web-Oberfläche und API
 from routes import *
 from routes_api import api
+
+# Registrierung des API-Blueprints in der Flask-App
 app.register_blueprint(api)
 
